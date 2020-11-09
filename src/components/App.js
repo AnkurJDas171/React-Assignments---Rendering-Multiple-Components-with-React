48d47bd3431cff79fa6bd5fde0def71a4fe57405
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-09 18:42:42 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">Color's Magic</h1>
+      <p data-ns-test="project-description">
+        Its an ecommerce site where customers can buy pre-painted painting or
+        can order a custom made painting for them. Sellers (or the artists) can
+        upload there pre-paintings details in there profile or can take orders
+        for custom paintings.
+      </p>
+    </>
+  );
+};
 
 export default App;
